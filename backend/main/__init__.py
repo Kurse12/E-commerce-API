faccb2353fe55370ca7aee84c5ab2237590636d0
Valentin <aether12@outlook.es>
2025-11-04 11:53:28 -0300

    db config

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -5,12 +5,29 @@ from dotenv import load_dotenv
 #Modulo para api-rest
 from flask_restful import Api
 
+#sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
 api = Api()
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     
     load_dotenv()
+    
+    #config db
+    PATH = os.getenv("DATABASE_PATH")
+    DB_NAME = os.getenv("DATABASE_NAME")
+    
+    if not os.path.exists(f'{PATH}/{DB_NAME}'):
+        os.chdir(f'{PATH}')
+        file = os.open(f'{DB_NAME}', os.O_CREAT)
+    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{PATH}{DB_NAME}'
+    db.init_app(app)
     
     import main.resources as resources
     api.add_resource(resources.ClientesResource, '/clientes')
