8f3ef8f619f1d33549e732988e3fdd23558885b5
Valentin <aether12@outlook.es>
2025-11-20 15:58:52 -0300

    clientes methods

--- backend/main/resources/Clientes.py
+++ backend/main/resources/Clientes.py
@@ -1,5 +1,7 @@
 from flask_restful import Resource
 from flask import jsonify, request
+from .. import db
+from main.models import UsuarioModel
 
 clientes = [
     {
@@ -17,23 +19,44 @@ clientes = [
 class Clientes(Resource):
     
     def get(self):
+        clientes = db.session.query(UsuarioModel).filter(UsuarioModel.role == 'cliente').all()
+        return jsonify({
+            'clientes': [clientes.to_json() for cliente in clientes]
+        })
         
-        return jsonify(
-            {
-            "clientes": clientes
-            }
-            )
     def post(self):
+        cliente = UsuarioModel.from_json(request.get_json())
+        cliente.role = 'cliente'
+        db.session.add(cliente)
+        db.session.commit()
+        return cliente.to_json(), 201
         
-        cliente = request.get_json()
-        clientes.append(cliente)
-        return cliente, 201
         
 class Cliente(Resource):
     def get(self, id):
+        cliente = db.session.query(UsuarioModel).get_or_404(id)
+        if cliente.role == 'cliente':
+            return cliente.to_json()
+        else:
+            return '', 404
         
-        return jsonify(
-            {
-                "cliente": clientes[int(id)]
-            }
-        )
+    def delete(self, id):
+        cliente = db.session.query(UsuarioModel).get_or_404(id)
+        try:
+            db.session.delete(cliente)
+            db.session.commit()
+            return '',204
+        except:
+            return '',404
+        
+    def put(self, id):
+        cliente = db.session.query(UsuarioModel).get_or_404(id)
+        data = request.get_json().items()
+        for key, value in data:
+            setattr(cliente, key, value)
+        try:
+            db.session.add(cliente)
+            db.session.commit()
+            return cliente.to_json(),201
+        except:
+            return '', 404
