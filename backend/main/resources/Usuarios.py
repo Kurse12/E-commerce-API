46d0807e752d9f860675bfcacb0ed8a5861000de
Valentin <aether12@outlook.es>
2025-11-20 15:49:31 -0300

    usuarios methods

--- backend/main/resources/Usuarios.py
+++ backend/main/resources/Usuarios.py
@@ -4,7 +4,27 @@ from .. import db
 from main.models import UsuarioModel
 
 class Usuario(Resource):
-    pass
+    def get(self, id):
+        usuario = db.session.query(UsuarioModel).get_or_404(id)
+        return usuario.to_json()
+    
+    def delete(self, id):
+        usuario = db.session.query(UsuarioModel).get_or_404(id)
+        db.session.delete(usuario)
+        db.session.commit()
+        return '', 204
+    
+    def put(self, id):
+        usuario = db.session.query(UsuarioModel).get_or_404(id)
+        data = request.get_json().items()
+        for key, value in data:
+            setattr(usuario, key, value)
+        db.session.add(usuario)
+        db.session.commit()
 
 class Usuarios(Resource):
-    pass
+    def get(self):
+        usuarios = db.session.query(UsuarioModel).all()
+        return jsonify({
+            'Usuarios': [usuario.to_json() for usuario in usuarios]
+        })
