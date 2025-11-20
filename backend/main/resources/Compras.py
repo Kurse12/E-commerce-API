4cea41cd2a4b3e3539b377474e07ac89baf5c8ed
Valentin <aether12@outlook.es>
2025-11-20 15:39:42 -0300

    compras methods

--- backend/main/resources/Compras.py
+++ backend/main/resources/Compras.py
@@ -4,7 +4,39 @@ from .. import  db
 from main.models import CompraModel
 
 class Compra(Resource):
-    pass
+    #buusca una compra por id, si no existe devuelve un 404, si existe intenta devolverla como json
+    def get(self,id):
+        compra = db.session.query(CompraModel).get_or_404(id)
+        try:
+            return compra.to_json()
+        except:
+            return '', 404
+    
+    #busca la compra por id, agarra el json recibido y acxtualiza cada atributo del modelo con setattr
+    #devuelve el objeto actualizado y estatus 201
+    def put(self,id):
+        compra = db.session.query(CompraModel).get_or_404(id)
+        data = request.get_json().items()
+        for key, value in data:
+            setattr(compra,key,value)
+        try:
+            db.session.add(compra)
+            db.session.commit()
+            return compra.to_json(),201
+        except:
+            return '', 404
 
 class Compras(Resource):
-    pass
+    #busca TODAS las compras devuelve una lista de json de cada una
+    def get(self):
+        compras = db.session.query(CompraModel).all()
+        return jsonify({
+            'compras': [compra.to_json() for compra in compras]
+        })
+        
+    #crea una compra
+    def post(self):
+        compra = CompraModel.from_json(request.get_json())
+        db.session.add(compra)
+        db.session.commir()
+        return compra.to_json(),201
--- backend/main/resources/Productos.py
+++ backend/main/resources/Productos.py
@@ -5,6 +5,7 @@ from .. import  db
 from main.models import ProductoModel
 
 class Producto(Resource):
+    #busca el producto por id, si existe devuelve el json
     def get(self, id):
         producto = db.session.query(ProductoModel).get_or_404(id)
         try:
@@ -12,7 +13,7 @@ class Producto(Resource):
         except:
             return 'Recurso no encontrado', 404
         
-        
+    # busca el producto por id, lee el json y actualiza cada atributo del modelo
     def put(self,id):
         producto = db.session.query(ProductoModel).get_or_404(id)
         data = request.get_json().items()
@@ -21,25 +22,30 @@ class Producto(Resource):
             
         try:
             db.session.add(producto)
+            db.session.commit()
         except:
             return "", 404
-        
+    
+    #busca el producto por id, elimina de la bd con delete
     def delete(self,id):
         producto = db.session.query(ProductoModel).get_or_404(id)
         try:
             db.session.delete(producto)
+            db.session.commit()
         except:
             return '', 404
 
 class Productos(Resource):
-    
+    #trae todos los productos
     def get(self):
         productos = db.session.query(ProductoModel).all()
         
         return jsonify({
             'productos': [producto.to_json() for producto in productos]
         })
-    
+        
+        
+    #agrega un producto
     def post(self):
         producto = ProductoModel.from_json(request.get_json())
         db.session.add(producto)
